869b5fe919aef0dfc289ba8b3bcab31aeebf165c
Eric Thilen <ericthilen2003@gmail.com>
2026-04-01 19:25:32 +0200

    Add DTO for ticket assignment

---
+++ src/main/java/org/example/cyberwatch/features/ticket/model/AssignTicketDTO.java
@@ -0,0 +1,17 @@
+package org.example.cyberwatch.features.ticket.model;
+
+public class AssignTicketDTO {
+
+    private Long staffId;
+
+    public AssignTicketDTO() {
+    }
+
+    public Long getStaffId() {
+        return staffId;
+    }
+
+    public void setStaffId(Long staffId) {
+        this.staffId = staffId;
+    }
+}
