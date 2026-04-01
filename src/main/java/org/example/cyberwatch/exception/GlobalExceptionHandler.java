987fd6ee96afdd7f259930d764f00d13fd8169d9
Eric Thilen <ericthilen2003@gmail.com>
2026-04-01 20:09:15 +0200

    Add exception handling for staff assignment

--- src/main/java/org/example/cyberwatch/exception/GlobalExceptionHandler.java
+++ src/main/java/org/example/cyberwatch/exception/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.example.cyberwatch.features.staff.exception.StaffNotFoundException;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -14,6 +15,13 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(TicketNotFoundException.class)
     public ResponseEntity<Map<String, String>> handleTicketNotFound(TicketNotFoundException e) {
+        Map<String, String> error = new HashMap<>();
+        error.put("message", e.getMessage());
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(StaffNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handleStaffNotFound(StaffNotFoundException e) {
         Map<String, String> error = new HashMap<>();
         error.put("message", e.getMessage());
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
