a598c39c96c2ca75392816f31e2032c9f963fcc8
Caroline Nordbrandt <caroline_nordbradt@hotmail.com>
2026-04-02 11:50:21 +0200

    Implement EmploymentFormService and ReportFormService with corresponding DTOs

--- src/main/java/org/example/cyberwatch/features/form/service/EmploymentFormService.java
+++ src/main/java/org/example/cyberwatch/features/form/service/EmploymentFormService.java
@@ -1,4 +1,22 @@
 package org.example.cyberwatch.features.form.service;
 
+import org.example.cyberwatch.features.form.repository.EmploymentFormRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class EmploymentFormService {
+
+    private final EmploymentFormRepository employmentFormRepository;
+
+    public EmploymentFormService(EmploymentFormRepository employmentFormRepository) {
+        this.employmentFormRepository = employmentFormRepository;
+    }
+
+    //Create employment & insert in staff.java
+
+    //view
+    //show all employmentforms with status waiting for approval
+
+    //delete
+    //When approved by management delete
 }
