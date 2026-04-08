95e66d75c761fc0cea31d8ff25ebee9c28d965d4
Younes <younes.ahmad@iths.se>
2026-04-08 09:14:33 +0200

    Resolved issue with cascade in Staff entity

--- src/main/java/org/example/cyberwatch/features/staff/model/Staff.java
+++ src/main/java/org/example/cyberwatch/features/staff/model/Staff.java
@@ -63,7 +63,7 @@ public class Staff {
     @OneToMany(mappedBy = "createdBy")
     private Set<EmploymentForm> createdForms;
 
-    @OneToMany(mappedBy = "assignedTo", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(mappedBy = "assignedTo")
     private Set<Ticket> assignedTickets = new HashSet<>();
 
     public Staff() {
