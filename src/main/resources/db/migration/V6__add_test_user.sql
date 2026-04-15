6015664f05ddc8af9c72b0cc755c2bf4d4f54923
Caroline Nordbrandt <caroline_nordbradt@hotmail.com>
2026-04-15 14:47:35 +0200

    Add test admin user "Caroline Nordbrandt" to database migration script

--- src/main/resources/db/migration/V6__add_test_user.sql
+++ src/main/resources/db/migration/V6__add_test_user.sql
@@ -1,3 +1,8 @@
 INSERT INTO staff (social_security_number, first_name, last_name, email, phone_number, role, department, password)
 VALUES ('19900101-1234', 'Eric', 'Thilen', 'ericthilen2003@gmail.com', '0701234567', 'ADMIN', 'MANAGEMENT', '$2a$10$w5pG/fM/yT.x/jZ0j2/H/.8yK5PzX3Y/O5F7y/6C6e9zX6y/O5F7y')
 ON CONFLICT (social_security_number) DO NOTHING; -- Avoid duplicate key errors if already present
+
+INSERT INTO staff (social_security_number, first_name, last_name, email, phone_number, role, department, password)
+VALUES ('19980508-4433', 'Caroline', 'Nordbrandt', 'nordbrandtcaroline@gmail.com', '0700889966', 'ADMIN', 'MANAGEMENT',
+        '$2a$12$I.sExmmE93znB/XxQAzkv.PPRYy338sjr6BBJAKjruCYSQQc31NGm')
+ON CONFLICT (social_security_number) DO NOTHING;
